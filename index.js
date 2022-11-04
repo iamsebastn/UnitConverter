5c12bbe2ff7406e74c59076b223f121107187a1b
Sebastian Wolf <sebastian.wolf298@gmail.com>
2022-11-04 19:01:21 +0100

    Added some adjustments

--- index.js
+++ index.js
@@ -3,22 +3,35 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+const conversions = {
+    meter: 3.281,
+    liter: 0.264,
+    kilo: 2.204
+}
 
 const convertBtn = document.getElementById("convert-btn")
+const inputEl = document.getElementById("input-el")
 
 convertBtn.addEventListener("click", function() {
-    console.log("Clicked")
+    convertLength()
+    convertVolume()
+    convertMass()
+
+    console.log(inputEl.value + " times Hello")
 })
 
 
 function convertLength() {
-
+    let output = (inputEl.value) * 3.281
+    console.log("Your unit is around " + output + " feet")
 }
 
 function convertVolume() {
-
+    let output = (inputEl.value) * 0.264
+    console.log("Your unit is around " + output + " gallons")
 }
 
 function convertMass() {
-
+    let output = (inputEl.value) * 2.204
+    console.log("Your unit is around " + output + " pounds")
 }
